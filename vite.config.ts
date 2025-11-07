56f46caca75ca5a47346d1423c64459285f771a5
Lee Robinson <Leerobinson@hotmail.co.uk>
2025-11-07 15:00:19 +0000

    Update vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/motoTrip/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(process.env.API_KEY || env.API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(process.env.API_KEY || env.API_KEY),
+        'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.GOOGLE_MAPS_API_KEY || env.GOOGLE_MAPS_API_KEY)
       },
       resolve: {
         alias: {
